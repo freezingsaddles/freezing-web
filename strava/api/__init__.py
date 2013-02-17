f2dfe57211df2adf8f063180eebf148b3e936f4f
Hans Lellelid <hans@xmpl.org>
2013-02-17 16:10:39 -0500

    Another missed py2.6 compat issue.

--- strava/api/__init__.py
+++ strava/api/__init__.py
@@ -18,7 +18,7 @@ class BaseServerProxy(object):
         self.log = logging.getLogger('{0.__name__}.{0.__module__}'.format(self.__class__))
     
     def _get(self, url, params=None):
-        self.log.debug("GET {!r} with params {!r}".format(url, params))
+        self.log.debug("GET {0!r} with params {1!r}".format(url, params))
         raw = requests.get(url, params=params)
         raw.raise_for_status()
         resp = self._handle_protocol_error(raw.json())
