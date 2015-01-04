29b090cb12fbe7bfbdcdb1d1b0c52d8642288f0b
Hans Lellelid <hans@xmpl.org>
2015-01-04 13:44:41 -0500

    code cleanup

--- bafs/views/people.py
+++ bafs/views/people.py
@@ -5,63 +5,67 @@ from datetime import date, timedelta
 from sqlalchemy import text
 from datetime import datetime
 
+
 def people_list_users():
-	users_list = db.session.query(Athlete).order_by(Athlete.name) # @UndefinedVariable
-	#tdy = date(2013, 2, 10) # For testing because DB is old
-	tdy = date.today()
-	week_start = tdy - timedelta(days=(tdy.weekday() + 1) % 7)
-	week_end = week_start + timedelta(days=6)
-	users = []
-	for u in users_list:
-		weekly_dist = 0
-		weekly_rides = 0
-		total_rides = 0
-		total_dist = 0
-		for r in u.rides:
-			total_rides += 1
-			total_dist += r.distance
-			if week_start <= r.start_date.date() <=week_end:
-				weekly_dist += r.distance
-				weekly_rides += 1
-		users.append({"name":u.display_name,
-			"id": u.id,
-			"weekrides":weekly_rides,
-			"weektotal":weekly_dist,
-			"totaldist": total_dist,
-			"totalrides": total_rides})
-	return render_template('people/list.html', users=users, weekstart=week_start, weekend=week_end)
-	
+    users_list = db.session.query(Athlete).order_by(Athlete.name)  # @UndefinedVariable
+    # tdy = date(2013, 2, 10) # For testing because DB is old
+    tdy = date.today()
+    week_start = tdy - timedelta(days=(tdy.weekday() + 1) % 7)
+    week_end = week_start + timedelta(days=6)
+    users = []
+    for u in users_list:
+        weekly_dist = 0
+        weekly_rides = 0
+        total_rides = 0
+        total_dist = 0
+        for r in u.rides:
+            total_rides += 1
+            total_dist += r.distance
+            if week_start <= r.start_date.date() <= week_end:
+                weekly_dist += r.distance
+                weekly_rides += 1
+        users.append({"name": u.display_name,
+                      "id": u.id,
+                      "weekrides": weekly_rides,
+                      "weektotal": weekly_dist,
+                      "totaldist": total_dist,
+                      "totalrides": total_rides})
+    return render_template('people/list.html', users=users, weekstart=week_start, weekend=week_end)
+
+
 def people_show_person(user_id):
-	our_user = db.session.query(Athlete).filter_by(id=user_id).first() # @UndefinedVariable
-	our_team = db.session.query(Team).filter_by(id=our_user.team_id).first() # @UndefinedVariable
-	tdy = date.today()
-	week_start = tdy - timedelta(days=(tdy.weekday() + 1) % 7)
-	week_end = week_start + timedelta(days=6)
-	weekly_dist = 0
-	weekly_rides = 0
-	total_rides = 0
-	total_dist = 0
-	for r in our_user.rides:
-		total_rides += 1
-		total_dist += r.distance
-		if week_start <= r.start_date.date() <=week_end:
-			weekly_dist += r.distance
-			weekly_rides += 1		
-	return render_template('people/show.html', data={
-		"user":our_user, 
-		"team":our_team,
-		"weekrides":weekly_rides,
-		"weektotal":weekly_dist,
-		"totaldist": total_dist,
-		"totalrides": total_rides})
+    our_user = db.session.query(Athlete).filter_by(id=user_id).first()  # @UndefinedVariable
+    our_team = db.session.query(Team).filter_by(id=our_user.team_id).first()  # @UndefinedVariable
+    tdy = date.today()
+    week_start = tdy - timedelta(days=(tdy.weekday() + 1) % 7)
+    week_end = week_start + timedelta(days=6)
+    weekly_dist = 0
+    weekly_rides = 0
+    total_rides = 0
+    total_dist = 0
+    for r in our_user.rides:
+        total_rides += 1
+        total_dist += r.distance
+        if week_start <= r.start_date.date() <= week_end:
+            weekly_dist += r.distance
+            weekly_rides += 1
+    return render_template('people/show.html', data={
+    "user": our_user,
+    "team": our_team,
+    "weekrides": weekly_rides,
+    "weektotal": weekly_dist,
+    "totaldist": total_dist,
+    "totalrides": total_rides})
+
 
 def ridedays():
-	q = text("""
+    q = text("""
 		SELECT a.id, a.display_name, count(b.ride_date) as rides, sum(b.distance) as miles, max(b.ride_date) as lastride
 		 FROM athletes a, daily_scores b where a.id = b.athlete_id group by b.athlete_id order by rides desc, miles desc, display_name
 		;
 		"""
-		)
-	total_days = datetime.now().timetuple().tm_yday
-	ride_days = [(x['id'], x['display_name'], x['rides'], x['miles'], x['lastride']>=date.today()) for x in db.session.execute(q).fetchall()]
-	return render_template('people/ridedays.html', ride_days =ride_days, num_days = total_days)
+    )
+    total_days = datetime.now().timetuple().tm_yday
+    ride_days = [(x['id'], x['display_name'], x['rides'], x['miles'], x['lastride'] >= date.today()) for x in
+                 db.session.execute(q).fetchall()]
+    return render_template('people/ridedays.html', ride_days=ride_days, num_days=total_days)
