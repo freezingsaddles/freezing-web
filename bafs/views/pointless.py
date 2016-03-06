da711bc60edfd6a8afaef1634fb7ba8d049a1e28
Jon Renaut <jrenaut@gmail.com>
2016-03-06 08:59:46 -0500

    Added leaderboard by team day wins

--- bafs/views/pointless.py
+++ bafs/views/pointless.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from flask import render_template, Blueprint
 from sqlalchemy import text
+from itertools import groupby
+from collections import defaultdict
 
 from bafs import app, db, data
 
@@ -107,3 +109,30 @@ def team_riders():
     )
     team_riders = [(x['name'], x['ride_days']) for x in db.session.execute(q).fetchall()]
     return render_template('alt_scoring/team_riders.html', team_riders=team_riders)
+
+@blueprint.route("/alt_scoring/team_daily")
+def team_daily():
+    q = text("""select a.ride_date, b.name as team_name, sum(a.points) as team_score from daily_scores a,
+        teams b where a.team_id=b.id
+        group by a.ride_date, b.name order by a.ride_date, team_score;"""
+    )
+    temp = [(x['ride_date'], x['team_name']) for x in db.session.execute(q).fetchall()]
+    temp = groupby(temp, lambda x:x[0])
+    team_daily = defaultdict(list)
+    team_total = defaultdict(int)
+    for date, team in temp:
+        score_list = enumerate([x[1] for x in team], 1)
+        for a,b in score_list:
+            if not team_daily.get(date):
+                team_daily[date] = {}
+            team_daily[date].update({a:b})
+            if not team_total.get(b):
+                team_total[b] = 0
+            team_total[b] += (a)
+    team_daily = [(a,b) for a,b in team_daily.iteritems()]
+    team_daily = sorted(team_daily)
+    #NOTE: team_daily calculated to show the scores for each day
+    # chart is too big to display, but leaving the calculation here just in case
+    team_total = [(b,a) for a,b in team_total.iteritems()]
+    team_total = sorted(team_total, reverse = True)
+    return render_template('alt_scoring/team_daily.html', team_total=team_total)
