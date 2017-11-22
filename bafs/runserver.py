256518ba0c414c3a075db608cc7a0345e79cf251
Hans Lellelid <hans@xmpl.org>
2017-11-21 20:55:09 -0500

    Listen on all interfaces (so docker port fwd works)

--- bafs/runserver.py
+++ bafs/runserver.py
@@ -5,7 +5,7 @@ from colorlog import ColoredFormatter
 
 def main():
     # Setup some *fancy* logging :)
-    
+
     ch = logging.StreamHandler()
     ch.setLevel(logging.DEBUG)
     formatter = ColoredFormatter(
@@ -20,23 +20,23 @@ def main():
                     'CRITICAL': 'red',
             }
     )
-    
+
     ch.setFormatter(formatter)
-    
+
     loggers = [app.logger, logging.getLogger('stravalib'),
                logging.getLogger('requests'), logging.root]
-    
+
     for l in loggers:
         l.setLevel(logging.DEBUG)
         l.addHandler(ch)
-        
+
     #app.logger.setLevel(logging.DEBUG)
     #app.logger.debug("debug message")
     #app.logger.info("info message")
     #app.logger.warn("warning message")
     #app.logger.error("error message")
-    
-    app.run(debug=True)
-    
+
+    app.run(host='0.0.0.0', debug=True)
+
 if __name__ == '__main__':
     main()
