6d951c72875c42b2574d885a9c00b99d12142022
Hans Lellelid <hans@xmpl.org>
2018-01-28 19:53:53 -0500

    Use new init_logging

--- freezing/web/runserver.py
+++ freezing/web/runserver.py
@@ -1,43 +1,11 @@
-import logging
-
 from freezing.web import app
-from colorlog import ColoredFormatter
+from freezing.web.config import init_logging
 
 
 def main():
-    # Setup some *fancy* logging :)
-
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-    formatter = ColoredFormatter(
-            "%(log_color)s%(levelname)-8s%(reset)s [%(name)s] %(message)s",
-            datefmt=None,
-            reset=True,
-            log_colors={
-                    'DEBUG':    'cyan',
-                    'INFO':     'green',
-                    'WARNING':  'yellow',
-                    'ERROR':    'red',
-                    'CRITICAL': 'red',
-            }
-    )
-
-    ch.setFormatter(formatter)
-
-    loggers = [app.logger, logging.getLogger('stravalib'),
-               logging.getLogger('requests'), logging.root]
-
-    for l in loggers:
-        l.setLevel(logging.DEBUG)
-        l.addHandler(ch)
-
-    #app.logger.setLevel(logging.DEBUG)
-    #app.logger.debug("debug message")
-    #app.logger.info("info message")
-    #app.logger.warn("warning message")
-    #app.logger.error("error message")
-
+    init_logging(color=True)
     app.run(host='0.0.0.0', debug=True)
+
 
 if __name__ == '__main__':
     main()
