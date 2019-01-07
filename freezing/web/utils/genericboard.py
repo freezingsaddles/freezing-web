3ca31e316871b8fd8d2118ceb33355030ddef3de
Richard Bullington-McGuire <richard.bullington.mcguire@gmail.com>
2019-01-07 00:43:25 -0500

    Use UTF-8 for yaml leaderboards, fix londonbridge
    Give me emojis or give me death

--- freezing/web/utils/genericboard.py
+++ freezing/web/utils/genericboard.py
@@ -92,7 +92,7 @@ def load_board_and_data(leaderboard) -> Tuple[GenericBoard, List[Dict[str, Any]]
     if not os.path.exists(path):
         raise ObjectNotFound("Could not find yaml board definition {}".format(path))
 
-    with open(path) as fp:
+    with open(path, 'rt', encoding='utf-8') as fp:
         doc = yaml.load(fp)
 
     schema = GenericBoardSchema()
