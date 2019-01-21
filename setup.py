d19f137f9617821a249ac02b1380bc24e5f4845f
Richard Bullington-McGuire <richard.bullington.mcguire@gmail.com>
2019-01-21 17:03:17 -0500

    Bump version for release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup, find_packages
 __authors__ = ['"Hans Lellelid" <hans@xmpl.org>']
 __copyright__ = "Copyright 2018 Hans Lellelid"
 
-version = '1.1.9'
+version = '1.1.10'
 
 long_description="""
 The freezing saddles cycling competition website/scoreboard.
