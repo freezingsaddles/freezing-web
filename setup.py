d67b3b5691234b1865cac8633af52e63cfbef4cb
Richard Bullington-McGuire <richard.bullington.mcguire@gmail.com>
2024-01-14 16:37:02 -0500

    Bump version for release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ __authors__ = [
 
 __copyright__ = "Copyright 2015 Hans Lellelid"
 
-version = "1.2.0"
+version = "1.2.1"
 
 long_description = """
 The freezing saddles cycling competition website/scoreboard.
