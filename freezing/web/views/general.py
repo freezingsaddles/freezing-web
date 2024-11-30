a8de700fbe40c379e350fb520e52b49229a64945
Richard Bullington-McGuire <richard.bullington.mcguire@gmail.com>
2024-11-29 23:04:09 -0500

    fmt with black

--- freezing/web/views/general.py
+++ freezing/web/views/general.py
@@ -202,9 +202,12 @@ def authorization():
         if not no_teams:
             auth.login_athlete(strava_athlete)
         # Thanks https://stackoverflow.com/a/32926295/424301 for the hint on tzinfo aware compares
+        after_competiton_start = (
+            datetime.now(config.START_DATE.tzinfo) > config.START_DATE
+        )
         return render_template(
             "authorization_success.html",
-            after_competiton_start=datetime.now(config.START_DATE.tzinfo) > config.START_DATE,
+            after_competiton_start=after_competition_start,
             athlete=strava_athlete,
             team=team,
             competition_teams_assigned=len(config.COMPETITION_TEAMS) > 0,
