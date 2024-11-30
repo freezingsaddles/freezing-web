992551575516c0f62c6721e65deba5816afdc05d
Richard Bullington-McGuire <richard.bullington.mcguire@gmail.com>
2024-11-30 00:00:17 -0500

    Flask was ok, version banner innards were not

--- bin/freeze.sh
+++ bin/freeze.sh
@@ -2,7 +2,7 @@
 #
 # freeze.sh
 #
-# Overwrite freezing/meta.py with the current commit, branch name and build date.
+# Overwrite freezing/web/version/py with the current commit, branch name and build date.
 
 # Set unofficial bash strict mode http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
@@ -12,7 +12,7 @@ IFS=$'\n\t'
 SHORT_SHA="$(echo "${GITHUB_SHA:-unknown}" | cut -c1-8)"
 GITHUB_REF_NAME=${GITHUB_REF_NAME:-unknown}
 
-cat <<EOF > "freezing/meta.py"
+cat <<EOF > "freezing/web/version.py"
 commit = "$SHORT_SHA"
 branch = "$GITHUB_REF_NAME"
 build_date = "$(date -u +'%Y%m%dT%H%M%SZ')"
