a94074d6daefc77a860bb78e9ce62d23134fa24f
Richard Bullington-McGuire <richard.bullington.mcguire@gmail.com>
2024-12-19 00:46:18 -0500

    Add scripts to run lint and fmt and docs on them

---
+++ bin/fmt.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo "Running formatters"
+echo "*** black ***"
+black freezing
+echo "*** isort ***"
+isort freezing
+echo "*** djlint ***"
+djlint --reformat freezing/web/templates
+
